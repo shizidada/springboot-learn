4591ba47c479a1ca64dee02905b5099394fb7fc3
shizidada <shizidada@gmail.com>
2018-11-03 10:28:39 +0800

    update

--- springboot-learn/src/main/java/org/learn/bean/DataSourceBO.java
+++ springboot-learn/src/main/java/org/learn/bean/DataSourceBO.java
@@ -1,12 +1,12 @@
-package org.learn.config;
+package org.learn.bean;
 
 //@Data
 //@NoArgsConstructor
 //@AllArgsConstructor
 //@Configuration
 //@ConfigurationProperties(prefix = "jdbc")
-//@PropertySource("classpath:/config/databaseconfig.properties")
-//public class ConfigurationDataSource {
+//@PropertySource("classpath:/config/db.dev.properties")
+//public class DataSourceBO {
 //    private String host;
 //    private Integer port;
 //    private String username;
--- springboot-learn/src/main/java/org/learn/web/controller/RabbitMQController.java
+++ springboot-learn/src/main/java/org/learn/web/controller/RabbitMQController.java
@@ -1,7 +1,7 @@
 package org.learn.web.controller;
 
 
-import org.learn.rabbit.HelloSender;
+import org.learn.rabbit.SimpleSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RabbitMQController {
 
     @Autowired
-    HelloSender helloSender;
+    SimpleSender helloSender;
 
     @RequestMapping("/hello")
     public void hello() {
