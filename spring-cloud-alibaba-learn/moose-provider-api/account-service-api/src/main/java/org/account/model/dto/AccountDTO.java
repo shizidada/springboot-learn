47c884d4aef4f42f8131fd3ad47127c5033d5709
shizidada <shizidada@gmail.com>
2020-03-11 23:26:57 +0800

    add: mybatis mapper

---
+++ spring-cloud-alibaba-learn/moose-provider-api/account-service-api/src/main/java/org/account/model/dto/AccountDTO.java
@@ -0,0 +1,22 @@
+package org.account.model.dto;
+
+import lombok.Data;
+
+/**
+ * <p>
+ * Description
+ * </p>
+ *
+ * @author taohua
+ * @version v1.0.0
+ * @date 2020 2020/3/11 22:58
+ * @see org.account.model
+ */
+
+@Data
+public class AccountDTO {
+
+  private Long accountId;
+
+  private String accountName;
+}
