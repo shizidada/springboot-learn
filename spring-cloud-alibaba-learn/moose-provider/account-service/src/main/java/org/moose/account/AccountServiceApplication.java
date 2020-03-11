d12553c8444a8a45eff114c49af8620b45a370ec
shizidada <shizidada@gmail.com>
2020-03-11 23:26:07 +0800

    add: mybatis config & logback

--- spring-cloud-alibaba-learn/moose-provider/account-service/src/main/java/org/moose/account/AccountServiceApplication.java
+++ spring-cloud-alibaba-learn/moose-provider/account-service/src/main/java/org/moose/account/AccountServiceApplication.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * <p>
@@ -19,6 +20,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableDubbo
+@EnableTransactionManagement
 public class AccountServiceApplication {
 
   public static void main(String[] args) {
--- spring-cloud-alibaba-learn/moose-provider/account-service/src/main/java/org/moose/account/service/impl/AuthServiceImpl.java
+++ spring-cloud-alibaba-learn/moose-provider/account-service/src/main/java/org/moose/account/service/impl/AuthServiceImpl.java
@@ -2,10 +2,10 @@ package org.moose.account.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.account.service.AuthService;
 import org.apache.dubbo.config.annotation.Service;
 import org.moose.account.config.GithubAuthorizeProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * <p>
@@ -20,11 +20,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Service(version = "1.0.0")
 @Slf4j
-public class AuthServiceImpl {
+public class AuthServiceImpl implements AuthService {
 
   @Autowired
   private GithubAuthorizeProperties githubAuthorizeProperties;
 
+  @Override
   public String getAuthUrl(String type) {
     String url = "";
     switch (type) {
