b74d1cc23daf033b999a1a9c115df0957494a648
JiangJing <jiangjing@wshifu.com>
2020-03-12 18:40:13 +0800

    add: provider project dependencies

--- spring-cloud-alibaba-learn/moose-provider/user-service/src/main/java/org/moose/user/UserServiceApplication.java
+++ spring-cloud-alibaba-learn/moose-provider/user-service/src/main/java/org/moose/user/UserServiceApplication.java
@@ -3,6 +3,8 @@ package org.moose.user;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * <p>
@@ -14,8 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020 2020/3/8 10:27
  * @see org.moose.user
  */
+
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableDubbo
+@EnableTransactionManagement
 public class UserServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
