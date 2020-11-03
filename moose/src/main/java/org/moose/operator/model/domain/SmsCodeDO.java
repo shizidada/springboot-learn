f0adc0261ada58e1ca22c470001e73b205f07ae0
JiangJing <jiangjing@wshifu.com>
2020-11-03 15:26:11 +0800

    feat: sms id

--- moose/src/main/java/org/moose/operator/model/domain/SmsCodeDO.java
+++ moose/src/main/java/org/moose/operator/model/domain/SmsCodeDO.java
@@ -1,45 +1,18 @@
 package org.moose.operator.model.domain;
 
+import lombok.Data;
+
 /**
  * @author taohua
  */
+@Data
 public class SmsCodeDO extends BaseDO {
+
+  private String smsId;
 
   private String phone;
 
   private String type;
 
   private String code;
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public void setCode(String code) {
-    this.code = code;
-  }
-
-  @Override public String toString() {
-    return "SmsCodeDO{" +
-        "phone='" + phone + '\'' +
-        ", type='" + type + '\'' +
-        ", code='" + code + '\'' +
-        '}';
-  }
 }
